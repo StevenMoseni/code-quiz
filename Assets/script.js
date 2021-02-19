eb3a12c6cdfff63fd47abbb12c68acd7a7df87c9
Steven Moseni <smoseni@hotmail.com>
2021-02-19 03:11:39 -0800

    updated format, add js

---
+++ Assets/script.js
@@ -0,0 +1,88 @@
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+  return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+  if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+      this.score++;
+  }
+
+  this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+  return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+  return this.answer === choice;
+}
+
+
+function populate() {
+  if(quiz.isEnded()) {
+      showScores();
+  }
+  else {
+      var element = document.getElementById("question");
+      element.innerHTML = quiz.getQuestionIndex().text;
+
+      var choices = quiz.getQuestionIndex().choices;
+      for(var i = 0; i < choices.length; i++) {
+          var element = document.getElementById("choice" + i);
+          element.innerHTML = choices[i];
+          guess("btn" + i, choices[i]);
+      }
+
+      showProgress();
+  }
+};
+
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function() {
+      quiz.guess(guess);
+      populate();
+  }
+};
+
+
+function showProgress() {
+  var currentQuestionNumber = quiz.questionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+  var gameOverHTML = "<h1>Result</h1>";
+  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+  var element = document.getElementById("quiz");
+  element.innerHTML = gameOverHTML;
+};
+
+var questions = [
+  new Question("What’s the diameter of a basketball hoop in inches?", ["12 inches", "11 inches","15 inches", "18 inches"], "18 inches"),
+  new Question("How many players are on a baseball team?", ["24", "12", "9", "10"], "9"),
+  new Question("In professional basketball, how high is the basketball hoop from the ground?",["11 feet", "9 feet","10 feet", "12 feet"], "10 feet"),
+  new Question("What do you call it when a player makes three back to back strikes in bowling?",
+  ["Turkey", "Chicken", "Eagle", "Donkey"], "Turkey"),
+  new Question("The Olympics are held every how many years",
+  ["2", "5", "3", "4"], "4")
+];
+
+var quiz = new Quiz(questions);
+
+populate();
